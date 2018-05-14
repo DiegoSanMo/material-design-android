9a78238696d366b29ae6872f78cd54581efb54b8
Diego Mosan <diego14290764@itcg.edu.mx>
2018-05-13 20:14:55 -0500

    Added control buttons.

--- app/src/main/java/com/example/diegomoreno/calculatormd/MainActivity.java
+++ app/src/main/java/com/example/diegomoreno/calculatormd/MainActivity.java
@@ -2,14 +2,26 @@ package com.example.diegomoreno.calculatormd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btUno, btDos, btTres, btCuatro, btCinco, btSeis, btSiete, btOcho, btNueve, btCero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        btCero = (Button)findViewById(R.id.btCero);
+        btUno = (Button)findViewById(R.id.btUno);
+        btDos = (Button)findViewById(R.id.btDos);
+        btTres = (Button)findViewById(R.id.btTres);
+        btCuatro = (Button)findViewById(R.id.btCuatro);
+        btCinco = (Button)findViewById(R.id.btCinco);
+        btSeis = (Button)findViewById(R.id.btSeis);
+        btSiete = (Button)findViewById(R.id.btSiete);
+        btOcho = (Button)findViewById(R.id.btOcho);
+        btNueve = (Button)findViewById(R.id.btNueve);
     }
 }
